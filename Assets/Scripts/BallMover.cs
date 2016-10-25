303183cd469638b3ff4059fbdced572b45c718ae
Sami-Joonas Saarinen <Sami-Joonas Saarinen>
2016-10-25 22:13:08 +0300

    ball and player

--- Assets/Scripts/BallMover.cs
+++ Assets/Scripts/BallMover.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class BallMover : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float speed = 1;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = -transform.forward * speed;
+
+    }
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // public variables
+    public float speed = 3.0f;
+    //public float gravity = 9.81f;
+
+    private CharacterController characterController;
+    private Rigidbody rigidbody;
+    private Vector3 positionYZ;
+
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        Vector3 movementX = Input.GetAxis("Horizontal") * Vector3.right * speed * Time.deltaTime;
+        
+        Vector3 movement = transform.TransformDirection(movementX);
+
+        
+        Debug.Log("Movement Vector = " + movement);
+
+        var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (x > -3 && x < 3)
+        {
+            transform.Translate(x, 0.0f, 0.0f);
+        }
+        // Actually move the character controller in the movement direction
+        //rigidbody.MovePosition(transform.position + movement);
+        //transform.position.Set(transform.position.x, 0.0f, 0.0f);
+    }
 }
